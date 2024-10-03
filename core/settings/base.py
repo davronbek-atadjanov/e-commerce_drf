4956fad09983dce50db5a18272bd34ce34076643
NItro <atadjanovdavronbek863@gmail.com>
2024-10-03 18:31:15 +0500

    Deploy uchun tayyorladik

--- core/settings/base.py
+++ core/settings/base.py
@@ -45,6 +45,9 @@ INSTALLED_APPS = LOCAL_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    # whitenoise ni shu yerga qo'shing
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    #
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -52,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "debug_toolbar.middleware.DebugToolbarMiddleware",
+
 ]
 
 DEBUG_TOOLBAR_CONFIG = {
@@ -133,11 +137,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    '/home/davronb3/davronbek-django-projects.uz/django/static_files'
+]
+STATIC_ROOT = '/home/davronb3/davronbek-django-projects.uz/django/static'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = '/home/davronb3/davronbek-django-projects.uz/django/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
